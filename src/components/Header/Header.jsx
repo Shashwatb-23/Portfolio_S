e55c6cd4e7f35d5120fce97e89c16cf184e9825f
Shashwat Bhardwaj <shashwatbhardwaj3@gmail.com>
2024-04-29 16:53:30 +0530

    refactoring css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,15 @@
 export default function Header(){
     return(
-        <sction id="home">
-            <nav  className="head p-3 w-full bg-zinc-900 bg-opacity-85">
-                <div className="heading_content">
-                    <ul className="nav-links flex justify-evenly align-middle">
-                        <li className="nav-list"><a href="#home">Home</a></li>
-                        <li className="nav-list"><a href="#about">About </a></li>
-                        <li className="nav-list"><a href="#skills">Skills</a></li>
-                        <li className="nav-list"><a href="#projects">Projects</a></li>
-                        <li className="nav-list"><a href="#contact">Contact </a></li>
-                    </ul>
-                </div>
-            </nav>
-        </sction>
+        <nav  className="head p-3 w-full bg-zinc-900 bg-opacity-85">
+            <div className="heading_content">
+                <ul className="nav-links flex justify-evenly align-middle">
+                    <li className="nav-list"><a href="#home">Home</a></li>
+                    <li className="nav-list"><a href="#about">About </a></li>
+                    <li className="nav-list"><a href="#skills">Skills</a></li>
+                    <li className="nav-list"><a href="#projects">Projects</a></li>
+                    <li className="nav-list"><a href="#contact">Contact </a></li>
+                </ul>
+            </div>
+        </nav>
     )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,11 @@
 export default function Home(){
     return(
         
-        <div id="home">
-            <div className="profile-container">
-                <img src="/Images/profile photo/IMG-20230121-WA0110.jpg" alt="profile-pic" className="profile-pic" />
+        <div id="home" className=" flex justify-center items-center">
+            <div className="profile-container my-2.5 mx-2 px-24 py-10">
+                <img src="/Images/profile photo/IMG-20230121-WA0110.jpg" alt="profile-pic" className="profile-pic w-64 rounded-2xl grayscale hover:grayscale-0 transition-all" />
             </div>
-            <div className="intro">
+            <div className="intro p-4 m-2 rounded-lg ">
                 <div className="myself">
                 <h3>Hello I&#39;m</h3>
                 <h2>Shashwat Bhardwaj</h2>
