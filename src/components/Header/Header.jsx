cca39732da27163888574897e99b290ad87c6eda
Shashwat Bhardwaj <shashwatbhardwaj3@gmail.com>
2024-04-30 15:21:05 +0530

    completed navbar, onlything which is remaining from navbar i.e to add the navlinks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,12 +5,12 @@ export default function Header(){
         <nav  className="head p-3 w-full bg-zinc-900 bg-opacity-85">
             <div className="heading_content ">
                 <ul className="nav-links flex justify-evenly align-middle">
-                    <li className=" text-lg font-bold"><a href="#home">Home</a></li>
-                    <li className=" text-lg font-bold"><a href="#about">About </a></li>
-                    <li className=" text-lg font-bold"><a href="#skills">Skills</a></li>
-                    <li className=" text-lg font-bold"><a href="#projects">Projects</a></li>
-                    <li className=" text-lg font-bold"><a href="#contact">Contact </a></li>
-                    <li className=" text-lg font-bold cursor-pointer pt-1"><BsFillMoonStarsFill /></li>
+                    <li className=" text-lg font-bold  hover:scale-150 transition-all"><a href="#home">Home</a></li>
+                    <li className=" text-lg font-bold  hover:scale-150 transition-all"><a href="#about">About </a></li>
+                    <li className=" text-lg font-bold  hover:scale-150 transition-all"><a href="#skills">Skills</a></li>
+                    <li className=" text-lg font-bold  hover:scale-150 transition-all"><a href="#projects">Projects</a></li>
+                    <li className=" text-lg font-bold  hover:scale-150 transition-all"><a href="#contact">Contact </a></li>
+                    <li className=" text-lg font-bold cursor-pointer pt-1 hover:scale-150 transition-all "><BsFillMoonStarsFill /></li>
                 </ul>
             </div>
         </nav>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,17 +5,18 @@ import { SiNetlify } from "react-icons/si";
 
 export default function Home(){
     return(
-        
+    <section>    
         <div id="home" className=" flex justify-center items-center">
-            <div className=" my-2.5 mx-2 px-24 py-10">
+            <div className=" my-10 mx-2 px-24 py-10">
                 <img src="/Images/profile photo/IMG-20230121-WA0110.jpg" alt="profile-pic" className=" w-64 rounded-2xl grayscale hover:grayscale-0 transition-all" />
             </div>
 
             <div className=" p-4 m-2 rounded-lg bg-zinc-900  text-xl hover:scale-105 transition-all">
+
                 <div className=" flex text-center flex-col justify-center gap-2">
-                <h3 className="text-base">Hello I&#39;m</h3>
-                <h2 className=" text-2xl">Shashwat Bhardwaj</h2>
-                <h1 className=" text-4xl">Frontend Web Developer</h1>
+                    <h3 className="text-base">Hello I&#39;m</h3>
+                    <h2 className=" text-2xl">Shashwat Bhardwaj</h2>
+                    <h1 className=" text-4xl">Frontend Web Developer</h1>
                 </div>
 
                 <div className=" flex mt-5 justify-evenly text-4xl ">
@@ -31,11 +32,16 @@ export default function Home(){
                     <div className=" cursor-pointer bg-gradient-to-r from-cyan-300 to-teal-800 p-3 rounded-lg">
                         <a href="https://app.netlify.com/teams/shashwatb-23/overview"><SiNetlify /></a>    
                     </div>
-
                 </div>
 
             </div>
-
         </div>
+
+        <div className="flex justify-center gap-10 mx-auto mt-10 bg-gradient-to-r from-gray-500 to-gray-950 w-5/12 py-3 opacity-90 hover:scale-105 transition-all rounded-lg">
+            <button className="m-1 p-1.5 px-5 text-black font-bold bg-gradient-to-r from-slate-50 to-slate-600 rounded-lg">Resume</button>
+            <button className="m-1 p-1.5 px-5 text-black font-bold bg-gradient-to-r from-slate-50 to-slate-600 rounded-lg">Contact-Info</button>
+        </div>
+
+    </section>
     )
 }
