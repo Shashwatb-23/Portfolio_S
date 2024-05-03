ecab4930eac02701ba00e1190720def586e70a3b
Shashwat Bhardwaj <shashwatbhardwaj3@gmail.com>
2024-05-03 23:07:56 +0530

    started about component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,14 @@
 export default function About(){
     return(
         <section>
-            <div>
-                
+            <div className="flex justify-center ">
+                <div>
+                    <img src="public/Images/profile photo/2.jpg" alt="2nd profile" className="w-16" />
+                    <h1>About</h1>
+                </div>
+                <div>
+                    
+                </div>
             </div>
         </section>
     )
