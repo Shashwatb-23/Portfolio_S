85f3d971cedf93465d7a9bce514a89adaac905a0
Shashwat Bhardwaj <shashwatbhardwaj3@gmail.com>
2024-06-05 14:51:23 +0530

    starting project component

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,8 +1,14 @@
 const Project = ()=>{
     return (
-        <div>
-            <h1>projects</h1>
+    <>
+        <div className="flex justify-center">
+            <h1 className="mt-10 text-4xl font-bold hover:text-blue-400 cursor-pointer">Projects</h1>
         </div>
+        {/* =========================================================================================== */}
+        <div className="bg-slate-600 bg-opacity-80 rounded-md p-3 pl-7 mt-8 mx-20">
+            
+        </div>
+    </>
     )
 }
 
