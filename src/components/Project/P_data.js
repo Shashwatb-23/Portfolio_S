c9c32c2e6b59266b226a096ffd4190d4d253fbbc
Shashwatb-23 <shashwatbhardwaj3@gmail.com>
2024-12-30 20:55:51 +0530

    resolved the small bug

--- src/components/Project/P_data.js
+++ src/components/Project/P_data.js
@@ -1,19 +1,19 @@
 const projects = [
     {
         title: "Meme Generator",
-        p_img:`public/Images/icons/Meme Generator.jfif`,
+        p_img:`/Images/icons/Meme Generator.jfif`,
         description: "A fun web application to create and customize memes.",
         link: "https://buzzwords-generator.netlify.app/",
     },
     {
         title: "Order Food Now",
-        p_img:`public/Images/icons/Order food.jpg`,
+        p_img:`/Images/icons/Order food.jpg`,
         description: "A user-friendly food ordering platform.",
         link: "https://orderfoodnow.netlify.app/",
     },
     {
         title: "Copy URL Extension",
-        p_img:`public/Images/icons/Copy URL.jpg`,
+        p_img:`/Images/icons/Copy URL.jpg`,
         description: "A browser extension to copy URLs effortlessly.",
         link: "https://shashwatb-23.github.io/Copy-URL--Extension-/",
     },
